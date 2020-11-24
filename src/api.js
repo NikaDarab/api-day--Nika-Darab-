762d419eee3c9519ec31729347b08d82342d2e8c
Nika Darab <nikadarab@Nikas-Air.attlocal.net>
2020-11-24 13:21:40 -0600

    GET request

--- src/api.js
+++ src/api.js
@@ -1,11 +1,17 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unreachable */
 /* eslint-disable no-unused-vars */
 
 
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ei-student';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/nika-darab';
 
 const getItems = function () {
-  return Promise.resolve('A successful response!');
+
+  return fetch(`${BASE_URL}/items`);
+
 };
+
+
 
 export default {
   getItems
